938833e182f8d688aa7b705461c44f2aab09f6ad
AMIRAALI1907 <121364191+AMIRAALI1907@users.noreply.github.com>
2023-02-21 14:43:38 +0200

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,33 @@
+
+
+
+// var allproducts=document.querySelectorAll(".list li");
+//         var div1=document.querySelector(".content");
+//         var btn1=document.querySelector(".bttnn");
+//         var div2=document.querySelector(".content2");
+//         var price=0;
+
+//         allproducts.forEach(function(item){
+
+//            item.onclick=function(){
+//             div1.innerHTML+=item.textContent+".";
+//             price+=parseInt(item.getAttribute("price"))
+
+
+//             if(div1.innerHTML != ""){
+//                 btn1.style.display="block";
+               
+//                 btn1.style.color="blue"
+
+//             }
+
+
+//            }
+
+//         })
+//         btn1.onclick=function(){
+//             div2.innerHTML=price;
+//             div2.style.visibility="visible"
+           
+            
+//         }
